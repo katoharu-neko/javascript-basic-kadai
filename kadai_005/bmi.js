5277f1e8679b7ed28a83fcc52d34901f3423b581
katoharu-neko <haruki.katoh.0@gmail.com>
2025-06-11 16:15:12 +0900

    kadai005アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,9 +1,9 @@
 //変数の定義
-let wight = 68 ;
+let weight = 68 ;
 let height = 1.7 ;
 
 //BMIの計算
-let bmi = wight / (height * height);
+let bmi = weight / (height * height);
 
 //BMIの表示
 console.log(bmi);
